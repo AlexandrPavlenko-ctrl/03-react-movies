810bea7b1ef63061a73c2305fc3d6eaccbbc4817
Oleksandr Pavlenko <aleksandr.pavlenko2016@gmail.com>
2026-06-21 11:34:24 +0300

    fix movie and movieService files

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,19 +1,27 @@
-import axios, { type AxiosResponse } from "axios";
-import type { MovieResponseData } from "../types/movie";
+import axios from "axios";
+import { type Movie } from "../types/movie";
+
+export interface MovieResponseData {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const TMDB_TOKEN = (import.meta as any).env.VITE_TMDB_TOKEN;
+
+const envVars = import.meta.env as unknown as Record<string, string>;
+const TMDB_TOKEN = envVars.VITE_TMDB_TOKEN;
 
 export const fetchMovies = async (
   query: string,
   page: number = 1,
 ): Promise<MovieResponseData> => {
-  const response: AxiosResponse<MovieResponseData> = await axios.get(BASE_URL, {
+  const response = await axios.get<MovieResponseData>(BASE_URL, {
     params: {
       query: query,
       page: page,
-      language: "uk-UA", // Поиск результатов на украинском языке
+      language: "uk-UA",
     },
     headers: {
       Authorization: `Bearer ${TMDB_TOKEN}`,
